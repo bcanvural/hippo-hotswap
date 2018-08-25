070e675032cd5a351090fab4ec1fcc0e5922c5d6
Baris Vural <bariscan.vural@bloomreach.com>
2018-08-25 12:10:34 +0200

    Initial commit

---
+++ site/src/main/java/com/bloomreach/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.bloomreach.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippohotswap:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
