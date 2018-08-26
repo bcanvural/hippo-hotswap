0734c7711b22727b8a58e35df3ed21cd84558fe9
Baris Vural <bariscan.vural@bloomreach.com>
2018-08-26 11:53:30 +0200

    Add support for essentials

--- deploy_local_tomcat.sh
+++ deploy_local_tomcat.sh
@@ -6,5 +6,5 @@ docker run -p 8080:8080 -p 8000:8000 \
 --mount type=bind,source="$(pwd)"/cms/target/classes,target=/usr/local/tomcat/cms-classes \
 --mount type=bind,source="$(pwd)"/site/target/classes,target=/usr/local/tomcat/site-classes \
 --mount type=bind,source="$(pwd)"/site/target/site.war,target=/usr/local/tomcat/webapps/site.war \
---mount type=bind,source="$(pwd)"/repository-data,target=/usr/local/tomcat/hippoproject/repository-data \
+--mount type=bind,source="$(pwd)",target=/usr/local/tomcat/hippoproject \
 bcanvural/hippo-hotswap:v1
