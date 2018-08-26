bbed5cb5671e15e89d39eee6be7dcd79c41eb3fa
Baris Vural <bariscan.vural@bloomreach.com>
2018-08-26 02:10:45 +0200

    Bind site war location inside container

--- deploy_local_tomcat.sh
+++ deploy_local_tomcat.sh
@@ -4,6 +4,7 @@ mvn -Pdist
 mvn -Pdocker.build
 docker run -p 8080:8080 -p 5005:5005 \
 --mount type=bind,source="$(pwd)"/cms/target/classes,target=/usr/local/tomcat/cms-classes \
---mount type=bind,source="$(pwd)"/cms/target/classes,target=/usr/local/tomcat/site-classes \
+--mount type=bind,source="$(pwd)"/site/target/classes,target=/usr/local/tomcat/site-classes \
+--mount type=bind,source="$(pwd)"/site/target/site.war,target=/usr/local/tomcat/webapps/site.war \
 --mount type=bind,source="$(pwd)"/repository-data,target=/usr/local/tomcat/hippoproject/repository-data \
 bcanvural/hippo-hotswap:v1
